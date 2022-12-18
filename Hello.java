e08ef95dc08ef0d7cd541ab9debb5705da1c486e
MinimalKushal <kushalchauhanonline@gmail.com>
2022-12-18 21:03:10 +0530

    2nd line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
          System.out.println("1st line"); 
+          System.out.println("2nd line"); 
     }
 }
